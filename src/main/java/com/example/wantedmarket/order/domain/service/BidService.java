727a3462fd2dd3b5b68132e56f68cb9af0a596e3
SoliDeoHoneretGloria <iphone_1st@naver.com>
2023-05-24 21:42:29 +0200

    feat: [007-Bidding-submit] 가격제시 판매가 등록

---
+++ src/main/java/com/example/wantedmarket/order/domain/service/BidService.java
@@ -0,0 +1,42 @@
+package com.example.wantedmarket.order.domain.service;
+
+import static com.example.wantedmarket.exception.ErrorCode.NOT_FOUND_AUCTION;
+import static com.example.wantedmarket.exception.ErrorCode.NOT_FOUND_USER;
+
+import com.example.wantedmarket.exception.CustomException;
+import com.example.wantedmarket.order.domain.controller.dto.BidDto;
+import com.example.wantedmarket.order.domain.model.Auction;
+import com.example.wantedmarket.order.domain.model.Bid;
+import com.example.wantedmarket.order.domain.repository.AuctionRepository;
+import com.example.wantedmarket.order.domain.repository.BidRepository;
+import com.example.wantedmarket.user.domain.model.User;
+import com.example.wantedmarket.user.domain.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BidService {
+  private final BidRepository bidRepository;
+  private final UserRepository userRepository;
+  private final AuctionRepository auctionRepository;
+
+  @Transactional
+  public Bid bidSave(String userId, Long auction_number, Long bidPrice){
+    User user = userRepository.findByUserId(userId).orElseThrow(
+        () -> new CustomException(NOT_FOUND_USER));
+    Auction auction = auctionRepository.findById(auction_number).orElseThrow(
+        () -> new CustomException(NOT_FOUND_AUCTION));
+
+    BidDto dto = new BidDto();
+    dto.setUser(user);
+    dto.setAuction(auction);
+    dto.setBid_record(bidPrice);
+
+    Bid bid = dto.toEntity();
+
+    return bidRepository.save(bid);
+  }
+
+}
